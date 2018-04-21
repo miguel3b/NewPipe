6409a40e8226c81660c8770ea65891fdc638d967
Christian Schabesberger <chris.schabesberger@mailbox.org>
2018-04-21 23:10:01 +0200

    reorder playqueue/localPlaylist classes

--- app/src/main/java/org/schabi/newpipe/player/playback/MediaSourceManager.java
+++ app/src/main/java/org/schabi/newpipe/player/playback/MediaSourceManager.java
@@ -15,12 +15,12 @@ import org.schabi.newpipe.player.mediasource.FailedMediaSource;
 import org.schabi.newpipe.player.mediasource.LoadedMediaSource;
 import org.schabi.newpipe.player.mediasource.ManagedMediaSource;
 import org.schabi.newpipe.player.mediasource.PlaceholderMediaSource;
-import org.schabi.newpipe.playqueue.PlayQueue;
-import org.schabi.newpipe.playqueue.PlayQueueItem;
-import org.schabi.newpipe.playqueue.events.MoveEvent;
-import org.schabi.newpipe.playqueue.events.PlayQueueEvent;
-import org.schabi.newpipe.playqueue.events.RemoveEvent;
-import org.schabi.newpipe.playqueue.events.ReorderEvent;
+import org.schabi.newpipe.player.playqueue.PlayQueue;
+import org.schabi.newpipe.player.playqueue.PlayQueueItem;
+import org.schabi.newpipe.player.playqueue.events.MoveEvent;
+import org.schabi.newpipe.player.playqueue.events.PlayQueueEvent;
+import org.schabi.newpipe.player.playqueue.events.RemoveEvent;
+import org.schabi.newpipe.player.playqueue.events.ReorderEvent;
 import org.schabi.newpipe.util.ServiceHelper;
 
 import java.util.Collections;
@@ -39,7 +39,7 @@ import io.reactivex.functions.Consumer;
 import io.reactivex.internal.subscriptions.EmptySubscription;
 import io.reactivex.subjects.PublishSubject;
 
-import static org.schabi.newpipe.playqueue.PlayQueue.DEBUG;
+import static org.schabi.newpipe.player.playqueue.PlayQueue.DEBUG;
 
 public class MediaSourceManager {
     @NonNull private final String TAG = "MediaSourceManager@" + hashCode();
